c1cd76e6484b28bf7da7f46104dc61c46c5bc896
sach-school <christopher.sa@student.onslow.school.nz>
2020-08-11 10:26:38 +1200

    Ran checkstyle and fixed some bugs when adding movies

--- MovieManager.java
+++ MovieManager.java
@@ -15,8 +15,11 @@ import java.util.HashMap;
 
 /** Manage all movie objects stored in hash-map
  * and provide a recommendation
+ * 
+ * @author SACH
+ * @version 11/08/2020
  */
-public class MovieManager{
+public class MovieManager {
 
     // Create Fields
     private String title;
@@ -26,82 +29,119 @@ public class MovieManager{
     private String bill;
     
     // Set constants
-    private static double WIDTH = 400;
-    private static double HEIGHT = 100;
-    private static double LEFT = 20;
+    private final double WIDTH = 400;
+    private final double HEIGHT = 100;
+    private final double LEFT = 20;
     private double top;
     
     // Setting up hashmap for movie storage
     HashMap<String, Movie> movieRatings = new HashMap<String, Movie>();
     
+    /**
+     * Constructor method for class MovieManager
+     */
     public MovieManager() {
-        UI.initialise();
+        //UI.initialise();
         
-        UI.addTextField("Title / Taitara", this::setTitle);
-        UI.addTextField("Genre / Momo", this::setGenre);
-        UI.addTextField("Director / Kaitohu", this::setDirector);
-        UI.addButton("Like / Matareka", this::setRatingGood);
-        UI.addButton("Dislike / Matakawa", this::setRatingBad);
+        //UI.addTextField("Title / Taitara", this::setTitle);
+        //UI.addTextField("Genre / Momo", this::setGenre);
+        //UI.addTextField("Director / Kaitohu", this::setDirector);
+        //UI.addButton("Like / Matareka", this::setRatingGood);
+        //UI.addButton("Dislike / Matakawa", this::setRatingBad);
         
-        UI.addButton("Submit / Tuku kōrero", this::submit);
-        UI.setDivider(0.3);
-        UI.setFontSize(20);
+        //UI.addButton("Submit / Tuku kōrero", this::submit);
+        //UI.setDivider(0.3);
+        //UI.setFontSize(20);
     }
     
+    /**
+     * Sets the rating based on user input
+     */
     private void setRatingGood() {
         rating = "Like";
     }
     
+    /**
+     * Sets rationg based on user input
+     */
     private void setRatingBad() {
         rating = "Dislike";
     }
     
+    /**
+     * Sets the title based on user input
+     */
     private void setTitle(String usertitle) {
         title = usertitle.trim();
     }
     
+    /**
+     * Sets the genre from user input
+     */
     private void setGenre(String usergenre) {
         genre = usergenre.trim();
     }
     
+    /**
+     * Sets director based on user input
+     */
     private void setDirector(String userdirector) {
         director = userdirector.trim();
     }
 
-    private void submit(){
+    /**
+     * Creates a movie class using the user defined variables
+     */
+    private void submit() {
         // if statement to check all fields are not empty
-        if(title != null && genre != null && director != null) {
+        if (title != null && genre != null && director != null &&
+            !title.equals("") && !genre.equals("") && !director.equals("")) {
             Movie test = new Movie(title, genre, director, rating);
             movieRatings.put(title, test);
             title = null;
             rating = "";
             drawMovie();
-        } else {
-            UI.println("Please fill in all the \nrequired feilds");
+        } 
+        else if (title == null || title.equals("")) {
+            UI.println("Please enter a title");
+        } 
+        else if (genre == null || genre.equals("")) {
+            UI.println("Please enter a genre");
+        } 
+        else if (director == null || director.equals("")) {
+            UI.println("Please enter a director");
         }
     }
     
+    /**
+     * Draws the movie objects to the graphics pane
+     */
     private void drawMovie() {
         top = 20;
         UI.clearGraphics();
         int j = 1;
         for (Movie i : movieRatings.values()) {
-                UI.drawRect(LEFT, top, WIDTH, HEIGHT);
-                UI.drawString(i.getTitle(), LEFT + 10, top + 30);
-                UI.drawString("Genre: " + i.getGenre(), LEFT + 10, top + 60);
-                UI.drawString("Director: " + i.getDirector(), LEFT + 10, top + 80);
-                if (i.getRating() == "Like") {
-                    UI.drawImage("images/thumbsUp.png", WIDTH-(LEFT*2), HEIGHT/2+top,
-                                50, 50);
-                } else if (i.getRating() == "Dislike") {
-                    UI.drawImage("images/thumbsDown.png", WIDTH-(LEFT*2), HEIGHT/2+top,
-                                50, 50);
-                }
-                top += 120;
+            UI.drawRect(LEFT, top, WIDTH, HEIGHT);
+            UI.drawString(i.getTitle(), LEFT + 10, top + 30);
+            UI.drawString("Genre: " + i.getGenre(), LEFT + 10, top + 60);
+            UI.drawString("Director: " + i.getDirector(), LEFT + 10, top + 80);
+            if (i.getRating().equals("Like")) {
+                UI.drawImage("images/thumbsUp.png", WIDTH - (LEFT * 2), 
+                            HEIGHT / 2 + top, 50, 50);
+            } 
+            else if (i.getRating().equals("Dislike")) {
+                UI.drawImage("images/thumbsDown.png", WIDTH - (LEFT * 2), 
+                            HEIGHT / 2 + top, 50, 50);
+            }
+            top += 120;
         }
         
     }
     
+    /**
+     * Main class
+     * @param args aguments for main class
+     */
     public static void main(String[] args) {
         MovieManager obj = new MovieManager();
     }
